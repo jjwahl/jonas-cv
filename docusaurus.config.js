00c200d1c6038a0c552f4374e03f3f2006867573
jjwahl <147632337+jjwahl@users.noreply.github.com>
2023-10-12 11:52:43 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,43 +1,34 @@
 module.exports = {
   title: 'Jonas Wahl CV',
   tagline: 'Sounding!',
-  url: 'https://your-docusaurus-site.com', // Update with your site URL
-  baseUrl: '/', // Update with your site's base URL
+  url: 'https://your-docusaurus-site.com',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico', // Set your favicon path
-  organizationName: 'JJWAHL', // Your GitHub organization or username
-  projectName: 'Jonas CV', // Your GitHub repository name
-
+  favicon: 'img/favicon.ico',
+  organizationName: 'JJWAHL',
+  projectName: 'Jonas CV',
   presets: [
     [
       '@docusaurus/preset-classic',
       {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'), // Set up your sidebar navigation
-          // Additional configuration for the 'docs' section
+          sidebarPath: require.resolve('./sidebars.js'),
         },
-        // Configure other sections as needed (e.g., 'blog', 'pages', etc.)
       },
     ],
   ],
-
   themeConfig: {
-	  prism: {
-    theme: lightCodeTheme,
-    darkTheme: darkCodeTheme,
-    networkBindAddress: '0.0.0.0',
     navbar: {
       items: [
         {
           type: 'doc',
           docId: 'cv',
           position: 'left',
-          label: 'My CV', // Match this label with your sidebar configuration
+          label: 'My CV',
         },
         // Add other navbar items as needed
       ],
     },
-    // ... (footer configuration)
   },
 };
